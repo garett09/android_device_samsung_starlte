0932adc859183e448f4ef53b7ecc28834528e500
Adrian Garett Sian <adriansian@gmail.com>
2020-12-28 00:29:34 +0800

    starlte: remove gapps

--- corvus_starlte.mk
+++ corvus_starlte.mk
@@ -25,9 +25,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Corvus stuff
 TARGET_BOOT_ANIMATION_RES := 1440
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
